9f7b47297f631e2100272e43617e1e1aa4402a62
marcusholloway17 <loganjean17@gmail.com>
2023-04-22 17:15:14 +0000

    handle any type of object of error

--- utils/notification.service.ts
+++ utils/notification.service.ts
@@ -1,20 +1,30 @@
-import { Injectable } from '@angular/core';
-import { httpResponses } from 'src/app/blocs/httpResponses';
+import { Injectable } from "@angular/core";
+import { httpResponses } from "src/app/blocs/httpResponses";
 import {
   AppUIStateProvider,
   UIStateStatusCode,
-} from 'src/app/views/partial/ui-state/core';
+} from "src/app/views/partial/ui-state/core";
 
 // helper to send notification to user UI
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class NotificationService {
   constructor(private UIState: AppUIStateProvider) {}
 
   error(error: any) {
-    for (const key in error.errors) {
-      this.UIState.endAction(error.error[key]['msg'], UIStateStatusCode.ERROR);
+    if (error?.errors && error.errors?.length) {
+      for (const key in error.errors) {
+        this.UIState.endAction(
+          error.error[key]["msg"],
+          UIStateStatusCode.ERROR
+        );
+      }
+    } else {
+      this.UIState.endAction(
+        error.error?.message ?? error.error?.msg,
+        UIStateStatusCode.ERROR
+      );
     }
   }
 
