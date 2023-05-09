cf9b22e7b57513335dabe0e57faceceef0f18a44
marcusholloway17 <loganjean17@gmail.com>
2023-05-09 13:38:43 +0000

    setting up content projection for content header component

--- content-header/content-header.component.ts
+++ content-header/content-header.component.ts
@@ -1,17 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit, ViewChild } from "@angular/core";
+import { FORM_CLIENT, FormsClient, ReactiveFormComponentInterface } from "@azlabsjs/ngx-smart-form";
 
 @Component({
-  selector: 'app-content-header',
-  templateUrl: './content-header.component.html',
-  styleUrls: ['./content-header.component.css']
+  selector: "app-content-header",
+  templateUrl: "./content-header.component.html",
+  styleUrls: ["./content-header.component.css"],
 })
 export class ContentHeaderComponent implements OnInit {
   @Input() title: string | undefined;
   @Input() description: string | undefined;
   @Input() hasReturn: boolean = false;
-  constructor() { }
 
-  ngOnInit(): void {
-  }
 
+  @ViewChild("formvalue", { static: false })
+  formvalue!: ReactiveFormComponentInterface;
+  // form$ = this.formsClient.get();
+
+  constructor(@Inject(FORM_CLIENT) private formsClient: FormsClient) {}
+
+  ngOnInit(): void {}
 }
