875d88518e9e44585961d70bd0833be3d25f0c3a
marcusholloway17 <loganjean17gmail.com>
2023-05-10 15:16:38 +0000

    content header first step

--- content-header/content-header.component.ts
+++ content-header/content-header.component.ts
@@ -1,5 +1,16 @@
-import { Component, Inject, Input, OnInit, ViewChild } from "@angular/core";
-import { FORM_CLIENT, FormsClient, ReactiveFormComponentInterface } from "@azlabsjs/ngx-smart-form";
+import {
+  Component,
+  Inject,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+} from "@angular/core";
+import {
+  FORM_CLIENT,
+  FormsClient,
+  ReactiveFormComponentInterface,
+} from "@azlabsjs/ngx-smart-form";
 
 @Component({
   selector: "app-content-header",
@@ -7,10 +18,24 @@ import { FORM_CLIENT, FormsClient, ReactiveFormComponentInterface } from "@azlab
   styleUrls: ["./content-header.component.css"],
 })
 export class ContentHeaderComponent implements OnInit {
+  /**
+   * @description title of the content header
+   *
+   */
   @Input() title: string | undefined;
+  /**
+   * @description content to display after the title
+   */
   @Input() description: string | undefined;
+  /**
+   * @description display a return button if true
+   */
   @Input() hasReturn: boolean = false;
 
+  /**
+   * custom template
+   */
+  @Input() customTemplateRef!: TemplateRef<unknown>;
 
   @ViewChild("formvalue", { static: false })
   formvalue!: ReactiveFormComponentInterface;
