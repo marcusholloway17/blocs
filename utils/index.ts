028e72eac4bb58e125ee23eca5d2df7af3db66c1
marcusholloway17 <loganjean17@gmail.com>
2023-07-01 16:38:09 +0000

    import dependant declarations

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,57 @@
-import { FormControl } from '@angular/forms';
-import { FormComponentInterface } from '@azlabsjs/ngx-smart-form';
+import { FormControl } from "@angular/forms";
+import { FormComponentInterface } from "@azlabsjs/ngx-smart-form";
+
+export const PHONE_NUMBER_SUFFIX = "@c.us";
+export const SUCCESS_ALERT: AlertStateType = {
+  type: "success",
+  msg: "Message envoyé !",
+  closeDelay: 3000,
+};
+export const ERROR_ALERT: AlertStateType = {
+  type: "danger",
+  msg: "Échec d'envoie !",
+  closeDelay: 3000,
+};
+
+export function sanitizePhoneNumber(phone: string): string {
+  let validatedPhone = phone
+    .trim()
+    .split("+")
+    .join("")
+    .split(" ")
+    .join("")
+    .split("-")
+    .join("");
+  if (!validatedPhone.endsWith(PHONE_NUMBER_SUFFIX)) {
+    validatedPhone = `${validatedPhone}${PHONE_NUMBER_SUFFIX}`;
+  }
+  return validatedPhone;
+}
+
+export type ImageMessage = {
+  chatId: string;
+  file: FileType;
+  caption?: string;
+};
+
+export type FileType = {
+  mimetype: string;
+  filename: string;
+  data: string | unknown;
+};
+
+export type AlertStateType = {
+  type: string;
+  msg?: string;
+  closeDelay?: number;
+};
 
 // convert snake string to camel string
 export const snakeToCamel = (str: string) => {
   str
     .toLowerCase()
     .replace(/([-_][a-z])/g, (group) =>
-      group.toUpperCase().replace('-', '').replace('_', '')
+      group.toUpperCase().replace("-", "").replace("_", "")
     );
 };
 
