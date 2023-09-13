79ce2bb655c5c8f1b2adddc7a26dadc7eae53c64
marcusholloway17 <loganjean17@gmail.com>
2023-09-13 09:11:52 +0000

    update search component

--- searcher/searcher.component.ts
+++ searcher/searcher.component.ts
@@ -8,24 +8,60 @@ import {
   OnInit,
   Output,
   ViewChild,
-} from '@angular/core';
-import { SearcherService } from './searcher.service';
+} from "@angular/core";
+import { SearcherService } from "./searcher.service";
 import {
   FORM_CLIENT,
   FormsClient,
   ReactiveFormComponentInterface,
-} from '@azlabsjs/ngx-smart-form';
-import { Observable, Subject, catchError, takeUntil, throwError } from 'rxjs';
-import { FormConfigInterface } from '@azlabsjs/smart-form-core';
-import { AppUIStateProvider } from 'src/app/views/partial/ui-state/core';
+} from "@azlabsjs/ngx-smart-form";
+import {
+  Observable,
+  Subject,
+  catchError,
+  takeUntil,
+  tap,
+  throwError,
+} from "rxjs";
+import { FormConfigInterface } from "@azlabsjs/smart-form-core";
+import { AppUIStateProvider } from "src/app/views/partial/ui-state/core";
+
+export type SearchConfigType = {
+  /**
+   * Provide a title for the form component
+   * @optional title Object
+   */
+  title?: string;
+  /**
+   * show form title section if true
+   * @optional query Object
+   */
+  showTitle?: boolean;
+  /**
+   * form id used to build the form
+   * @required formId number | string
+   */
+  formId: number | string;
+  /**
+   * Provide a query if you want to apply a filter
+   * @optional query Object
+   */
+  query?: any;
+};
 
 @Component({
-  selector: 'app-searcher',
-  templateUrl: './searcher.component.html',
-  styleUrls: ['./searcher.component.css'],
+  selector: "app-searcher",
+  templateUrl: "./searcher.component.html",
+  styleUrls: ["./searcher.component.css"],
   providers: [SearcherService],
 })
 export class SearcherComponent implements OnInit, AfterContentInit, OnDestroy {
+  private DEFAULT_CONFIG: SearchConfigType = {
+    title: "Search",
+    showTitle: false,
+    formId: 0,
+  };
+
   /**
    * request url setter
    * @required url string
@@ -35,21 +71,20 @@ export class SearcherComponent implements OnInit, AfterContentInit, OnDestroy {
   }
 
   /**
-   * form id to make a request
-   * @required form_id string | number
+   * search config
+   * @optional config
    */
-  @Input() form_id: number | string = 0;
+  @Input() config: SearchConfigType = this.DEFAULT_CONFIG;
 
-  /**
-   * Provide a query if you want to apply a filter
-   * @optional query Object
-   */
-  @Input() query?: any;
-
-  @ViewChild('formvalue', { static: false })
+  @ViewChild("formvalue", { static: false })
   formvalue!: ReactiveFormComponentInterface;
   form$!: Observable<FormConfigInterface>;
   private destroy$ = new Subject();
+
+  /**
+   * Emit the search form value
+   */
+  @Output() submit = new EventEmitter();
 
   /**
    * Emit the result of the search
@@ -64,36 +99,87 @@ export class SearcherComponent implements OnInit, AfterContentInit, OnDestroy {
   constructor(
     @Inject(FORM_CLIENT) private formsClient: FormsClient,
     private searcherService: SearcherService,
-    private UIState: AppUIStateProvider
+    private uistate: AppUIStateProvider
   ) {}
 
-  ngOnInit(): void {}
-
-  async ngAfterContentInit(): Promise<void> {
-    this.form$ = this.formsClient.get(this.form_id);
+  ngOnInit(): void {
+    console.log("url", this.url);
   }
 
+  async ngAfterContentInit(): Promise<void> {
+    this.form$ = this.formsClient.get(this.config.formId);
+  }
+
+  /**
+   * on form ready state
+   * @param event form ready state event emitted
+   */
+  onReadyState(event: any) {}
+
+  /**
+   * submit the form data
+   * @param event
+   */
   onSubmit(event: any) {
-    this.UIState.startAction();
-    this.performingRequest.emit(true);
+    if (this.url) {
+      this.startLoader();
+      this.searcherService
+        .search({ ...event, ...this.config.query })
+        .pipe(
+          takeUntil(this.destroy$),
+          catchError((err) => {
+            this.stopLoader();
+            return throwError(() => err);
+          })
+        )
+        .subscribe((res: any) => {
+          this.onResult.emit(res);
+          this.stopLoader();
+        });
+    } else {
+      this.submit.emit(event);
+    }
+  }
+
+  /**
+   * perform custom search with the form data
+   * @param data query params data
+   * @param url query params data
+   */
+  customSearch(data: any = {}, url?: string) {
+    this.startLoader();
     this.searcherService
-      .search({ ...event, ...this.query })
+      .customSearch(data, url)
       .pipe(
         takeUntil(this.destroy$),
         catchError((err) => {
-          this.UIState.endAction();
-          this.performingRequest.emit(false);
+          this.stopLoader();
           return throwError(() => err);
-        })
+        }),
+        tap(
+          (response: any) => (this.onResult.emit(response), this.stopLoader())
+        )
       )
-      .subscribe((res: any) => {
-        this.onResult.emit(res);
-        this.UIState.endAction();
-        this.performingRequest.emit(false);
-      });
+      .subscribe();
   }
 
   ngOnDestroy(): void {
     this.destroy$.next(true);
+  }
+
+  /**
+   * start loading state
+   */
+  private startLoader() {
+    this.uistate.startAction();
+    this.performingRequest.emit(true);
+  }
+
+  /**
+   * stop loading state
+   */
+  private stopLoader() {
+    this.uistate.endAction();
+    this.performingRequest.emit(false);
   }
 }
