606fbac4c92daedcf32c8a460264326ed3416665
marcusholloway17 <loganjean17@gmail.com>
2023-09-27 13:41:39 +0000

    listening to changes in navigation before push it in the history

--- utils/navigation.service.ts
+++ utils/navigation.service.ts
@@ -1,6 +1,7 @@
 import { Location } from "@angular/common";
 import { Injectable } from "@angular/core";
 import { NavigationEnd, Router } from "@angular/router";
+import { distinctUntilChanged } from "rxjs";
 
 @Injectable({
   providedIn: "root",
@@ -9,7 +10,7 @@ export class NavigationService {
   private history: string[] = [];
 
   constructor(private router: Router, private location: Location) {
-    this.router.events.subscribe((event) => {
+    this.router.events.pipe(distinctUntilChanged()).subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.history.push(event.urlAfterRedirects);
       }
